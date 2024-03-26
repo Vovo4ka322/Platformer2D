851d748fa0b897b75ee02b7bf3070d9b2255f2c6
Vovo4ka322 <142627576+Vovo4ka322@users.noreply.github.com>
2024-03-26 22:28:09 +0300

    -

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,17 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] Transform[] _wayPoints;
+    [SerializeField] SpriteRenderer _spriteRenderer;
     [SerializeField] private float _moveSpeed;
 
     private int _currentPoint = 0;
-    
+
     private void Update()
     {
         Move();
+        Flip();
     }
 
     private void Move()
@@ -20,5 +20,13 @@ public class Enemy : MonoBehaviour
             _currentPoint = (_currentPoint + 1) % _wayPoints.Length;
 
         transform.position = Vector2.MoveTowards(transform.position, _wayPoints[_currentPoint].position, _moveSpeed * Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+        if (transform.position.x > _wayPoints[_currentPoint].position.x)
+            _spriteRenderer.flipX = false;
+        else
+            _spriteRenderer.flipX = true;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _jumpForce;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _ground;
+    [SerializeField] private float _groundCheckRadius;
+
+    private const string Horizontal = nameof(Horizontal);
+    private const string HorizontalMove = nameof(HorizontalMove);
+    private const string IsJumping = nameof(IsJumping);
 
     private Rigidbody2D _rigidbody2D;
     private Vector2 _position;
-
-    private const string Horizontal = nameof(Horizontal);
+    private float _horizontalMove;
+    private bool _isGround;
+    private int _jumpCount = 0;
+    private int _maxJumpCount = 2;
 
     private void Awake()
     {
@@ -21,7 +29,9 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         Move();
+        Flip();
         Jump();
+        PlayAnimation();
     }
 
     public void Move()
@@ -32,7 +42,26 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        _isGround = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _ground);
+
+        if (Input.GetKeyDown(KeyCode.Space) && (_isGround || (++_jumpCount < _maxJumpCount)))
             _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpForce);
+
+        if (_isGround)
+            _jumpCount = 0;
+    }
+
+    private void Flip()
+    {
+        if (_position.x > 0)
+            _spriteRenderer.flipX = false;
+        else
+            _spriteRenderer.flipX = true;
+    }
+
+    private void PlayAnimation()
+    {
+        _horizontalMove = Input.GetAxis(Horizontal);
+        _animator.SetFloat(HorizontalMove, Mathf.Abs(_horizontalMove));
     }
 }
